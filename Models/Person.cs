b25c31219932451486e2c5bad1a71dfcd3b2f47d
Dustin Engstrom <dengstr@wgu.edu>
2013-08-23 10:24:30 -0600

    Removed unused using directives

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
 
 namespace KendoUI.Models
 {
